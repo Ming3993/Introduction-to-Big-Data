255a2328cbbf43c0ca7b32b21367f718e616bf63
Nghiadz2004 <huunghia122004@gmail.com>
2025-03-28 12:23:27 +0700

    update Mapper

--- Lab 1/22120227/src/Mapper.py
+++ Lab 1/22120227/src/Mapper.py
@@ -6,16 +6,13 @@ target_letters = ['a', 'f', 'j', 'g', 'h', 'c', 'm', 'u', 's']
 
 # Đọc từng dòng từ input (words.txt)
 for line in sys.stdin:
-    words = line.strip().split()    # Tách từng từ trong dòng
+    # Tách các từ, bao gồm cả việc tách từ có ký tự đặc biệt
+    words = re.findall(r'[a-zA-Z]+', line)  # Chỉ lấy các phần có chữ cái
 
     # Lặp qua từng từ
     for word in words:
-        # Kiểm tra xem có ký tự đặc biệt không
-        if re.search(r'[^a-zA-Z]', word):
-            continue
-
         first_letter = word[0].lower()  # Lấy chữ cái đầu tiên và chuyển về chữ thường
 
         # Kiểm tra chữ cái đầu tiên có trong danh sách target_letters
         if first_letter in target_letters:
-            print(f"{first_letter}\t1") # Xuất ra dạng "chữ cái đầu \t 1" để truyền sang Reducer
+            print(f"{first_letter}\t1")  # Xuất ra dạng "chữ cái đầu \t 1" để truyền sang Reducer
