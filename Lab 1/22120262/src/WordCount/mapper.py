ffac5e3be8824bde8bc4330f42212663de4ca4f6
ThunderRed1578 <116338376+ThunderRed1578@users.noreply.github.com>
2025-03-29 18:55:29 +0700

    Delete Lab 1/22120262/src/WordCount/mapper.py

--- Lab 1/22120262/src/WordCount/mapper.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-key_set = {'a', 'f', 'j', 'g', 'h', 'c', 'm', 'u', 's'}  
-
-for line in sys.stdin:
-    line = line.strip()
-    if line[0].lower() in key_set and line.isalpha():
-        print(f"{line[0].lower()}\t{line}")
