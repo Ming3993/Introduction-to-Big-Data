74818daa855fe060eb8765733656357f1f0387f1
Ming3993 <minhlvn.t2.1922@gmail.com>
2025-03-29 20:12:22 +0700

    Update mapper logic and results

--- Lab 1/22120210/src/WordCount/mapper.py
+++ Lab 1/22120210/src/WordCount/mapper.py
@@ -1,21 +1,11 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python3
+import sys
+import re
 
-# import sys because we need to read and write data to STDIN and STDOUT 
-import sys 
+letters_set = {'a', 'f', 'g', 'j', 'h', 'c', 'm', 'u', 's'}
 
-letters_dict = {'a','f','g','j','h','c','m','u','s'}
-
-def Is_a_word(line):
-    for letter in line:
-        if not letter.isalpha():
-            return False
-    return True
-
-# reading entire line from STDIN (standard input) 
-for line in sys.stdin: 
-	# to remove leading and trailing whitespace 
-	line = line.strip() 
-	if line and line[0].lower() in letters_dict:
-		if not Is_a_word(line):
-			continue
-		print (f"{line[0].lower()}\t1")
+for line in sys.stdin:
+    words = re.split(r"[^a-zA-Z]+", line.strip())
+    for word in words:
+        if word and word[0].lower() in letters_set:
+            print(f"{word[0].lower()}\t1")
