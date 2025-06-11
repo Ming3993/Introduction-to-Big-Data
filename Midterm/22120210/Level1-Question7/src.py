3b119e39385b3b16075597dfe281ff7a5af9ad3d
Nghiadz2004 <huunghia122004@gmail.com>
2025-06-11 23:31:05 +0700

    Delete src in quesiton 7

--- Midterm/22120210/Level1-Question7/src.py
+++ Midterm/22120210/Level1-Question7/src.py
@@ -1,20 +1,19 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
-class MRCountNumbers(MRJob):
-
-    def mapper(self, _, line):
+class MRHistogram(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.exist_mapper,
+                   combiner=self.combiner,
+                   reducer=self.reducer),
+            MRStep(reducer=self.non_exist_reducer)
+        ]
+    def exist_mapper(self, _, line):
         numbers = sorted(line.strip().split(), key=int)
-        for num in range(int(numbers[0])):
-            yield num, 0
             
-        for i in range(len(numbers) - 1):
-            num = int(numbers[i])
+        for num in numbers:
             yield num, 1
-            while (num + 1 < int(numbers[i + 1])):
-                num += 1
-                yield num, 0
-        
-        yield int(numbers[-1]), 1
 
     def combiner(self, key, values):
         # Tổng tạm các giá trị (1) trong từng mapper
@@ -22,7 +21,20 @@ class MRCountNumbers(MRJob):
 
     def reducer(self, key, values):
         # Tổng toàn cục sau combiner
-        yield key, sum(values)
-
+        yield None, (key, sum(values))
+        
+    def non_exist_reducer(self, _, values):
+        hist = {}
+        max_hist = -1
+        for key, count in values:
+            val = int(key)
+            hist[val] = count
+            yield val, count
+            if max_hist < val:
+                max_hist = val
+        for i in range(max_hist + 1):
+            if i not in hist:
+                yield i, 0
+        
 if __name__ == '__main__':
-    MRCountNumbers.run()
+    MRHistogram.run()
